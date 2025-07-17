b6cc9e4b2042cc0d33597583f82df1ab9e2cce4b
Rohan Asawale <asawalerohan234@gmail.com>
2025-07-17 20:16:17 +0530

    uploading frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+        <LatestCollection />
+    </div>
+  )
+}
+
+export default Home
